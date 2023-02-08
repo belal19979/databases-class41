df1c0dfdb9618140136c5664ed0cde4d38b683c0
mohamed belal <mhbbelal22@gmail.com>
2023-02-08 13:58:45 +0100

    corrections w1-homework

--- Week1/homework/exercise1.js
+++ Week1/homework/exercise1.js
@@ -3,23 +3,46 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
-  database: 'meetup'
 });
 
-connection.connect(() => console.log('mySql Connected'));
+connection.connect(function (err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
 
-connection.query('DROP DATABASE IF EXISTS meetup');
-connection.query('CREATE DATABASE meetup', () => { console.log('meet up created') });
+  console.log('Connected to the MySQL server.');
+});
+
+connection.query('DROP DATABASE IF EXISTS meetup', function (error, results) {
+  if (error) throw error;
+  console.table(results);
+});
+connection.query('CREATE DATABASE meetup', function (error, results) {
+  if (error) throw error;
+  console.table(results);
+});
 connection.query('USE meetup');
 
-let inviteeSql = "CREATE TABLE Invitee  (invitee_no INT, invitee_name VARCHAR(50),invited_by VARCHAR(50) )";
-connection.query(inviteeSql)
+let inviteeSql =
+  'CREATE TABLE Invitee  (invitee_no INT, invitee_name VARCHAR(50),invited_by VARCHAR(50) )';
+connection.query(inviteeSql, function (error, results) {
+  if (error) throw error;
+  console.table(results);
+});
 
-let roomSql = "CREATE TABLE Room  (room_no INT, room_name VARCHAR(50),floor_number INT )";
-connection.query(roomSql)
+let roomSql =
+  'CREATE TABLE Room  (room_no INT, room_name VARCHAR(50),floor_number INT )';
+connection.query(roomSql, function (error, results) {
+  if (error) throw error;
+  console.table(results);
+});
 
-let meetingSql = "CREATE TABLE Meeting (meeting_no INT, meeting_title VARCHAR(50) , starting_time DATETIME, ending_time DATETIME, room_no INT)"
-connection.query(meetingSql)
+let meetingSql =
+  'CREATE TABLE Meeting (meeting_no INT, meeting_title VARCHAR(50) , starting_time DATETIME, ending_time DATETIME, room_no INT)';
+connection.query(meetingSql, function (error, results) {
+  if (error) throw error;
+  console.table(results);
+});
 
 connection.query(
   `INSERT INTO Invitee 
@@ -29,6 +52,11 @@ connection.query(
             (3, 'fede', 'belal'),
             (4, 'alex', 'belal'),
             (5, 'anas', 'belal')`,
+
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
 );
 
 connection.query(
@@ -39,6 +67,11 @@ connection.query(
          (3, 'Room 3', 3),
          (4, 'Room 4', 4),
          (5, 'Room 5', 5)`,
+
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
 );
 
 connection.query(
@@ -49,4 +82,9 @@ connection.query(
         (3, 'social Interview', '2022-01-09 09:00:00', '2022-01-10 17:00:00', 3),
         (4, 'tech Interview', '2022-01-11 09:00:00', '2022-01-12 17:00:00', 4),
         (5, 'Entertainment', '2022-01-13 09:00:00', '2022-05-14 17:00:00', 5)`,
+
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
 );
--- Week1/homework/exercise2.js
+++ Week1/homework/exercise2.js
@@ -3,29 +3,93 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
-  database: 'new_World'
+  database: 'new_World',
 });
 
-connection.connect(() => console.log('mySql Connected'));
+connection.connect(function (err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
 
-connection.query('SELECT Name FROM country WHERE Population > 8000000;');
+  console.log('Connected to the MySQL server.');
+});
 
-connection.query("SELECT Name FROM country WHERE Name LIKE '%land%'");
+connection.query(
+  'SELECT Name FROM country WHERE Population > 8000000;',
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
+
+connection.query(
+  "SELECT Name FROM country WHERE Name LIKE '%land%'",
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
 
 connection.query(
   'SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000',
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
 );
 
-connection.query("SELECT Name FROM country WHERE Continent = 'Europe'");
+connection.query(
+  "SELECT Name FROM country WHERE Continent = 'Europe'",
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
 
-connection.query('SELECT Name FROM country ORDER BY SurfaceArea DESC');
+connection.query(
+  'SELECT Name FROM country ORDER BY SurfaceArea DESC',
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
 
-connection.query("SELECT Name FROM city WHERE CountryCode = 'NLD'");
+connection.query(
+  "SELECT Name FROM city WHERE CountryCode = 'NLD'",
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
 
-connection.query("SELECT Population FROM city WHERE Name = 'Rotterdam'");
+connection.query(
+  "SELECT Population FROM city WHERE Name = 'Rotterdam'",
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
 
-connection.query('SELECT * FROM country ORDER BY SurfaceArea DESC LIMIT 10');
+connection.query(
+  'SELECT * FROM country ORDER BY SurfaceArea DESC LIMIT 10',
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
 
-connection.query('SELECT * FROM city ORDER BY Population DESC LIMIT 10');
+connection.query(
+  'SELECT * FROM city ORDER BY Population DESC LIMIT 10',
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
 
-connection.query('SELECT SUM(Population) FROM country');
+connection.query(
+  'SELECT SUM(Population) FROM country',
+  function (error, results) {
+    if (error) throw error;
+    console.table(results);
+  },
+);
